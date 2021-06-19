db364fb652e91b50f44967b10c2bbac3cdc86bac
somukhannan <somukhannan8@gmail.com>
2021-06-19 08:37:07 +0000

    animation added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 const App = () =>
 	<div className="App">
-		<div className="box b1">1</div>
-		<div className="box b2">2</div>
-		<div className="box b3">3</div>
-		<div className="box b4">4</div>
+		<div className="container">
+			<div className="box b1">1</div>
+			<div className="box b2">2</div>
+			<div className="box b3">3</div>
+			<div className="box b4">4</div>
+		</div>
 	</div>
 	;
 
