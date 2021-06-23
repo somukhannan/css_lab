a888afd570d04d75b9a1e5c591f073ecb8960156
Somukhannan Thiyagarajan <s.thiyagarajan@lakeba.com>
2021-06-23 18:34:04 +0530

    Shadow the logo done.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 const App = () =>
 	<div className="App">
 		<div className="container">
-			<div className="box b1">1</div>
-			<div className="box b2">2</div>
-			<div className="box b3">3</div>
-			<div className="box b4">4</div>
+			<div className="box b1 "/>
+			<div className="box b2 "/>
+			<div className="box b3 "/>
+			<div className="box b4 "/>
+		</div>
+		<div className="container-shadow">
+			<div className="box ba1 "/>
+			<div className="box ba2 "/>
+			<div className="box ba3 "/>
+			<div className="box ba4 "/>
 		</div>
 	</div>
 	;
