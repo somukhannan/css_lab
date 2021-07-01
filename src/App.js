cd22b6ab3c9f11ebb4a20f5cf57ddaecbc8052ab
somukhannan <somukhannan8@gmail.com>
2021-07-01 14:11:36 +0000

    Introduced Pendulum.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 
 const App = () =>
 	<div className="App">
-		<div className="container">
-			<div className="box b1 "/>
-			<div className="box b2 "/>
-			<div className="box b3 "/>
-			<div className="box b4 "/>
+		<div className="pendulum">
+			<div className="container">
+				<div className="line"/>
+				<div className="circle"/>
+			</div>
 		</div>
-	</div>
-	;
+	</div>;
 
 export default App;
+
+// <div className="container">
+// <div className="box b1 "/>
+// <div className="box b2 "/>
+// <div className="box b3 "/>
+// <div className="box b4 "/>
