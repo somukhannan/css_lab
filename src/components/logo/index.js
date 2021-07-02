ee4c7e77ec7d6d9f676f14461718c3d06810ac5c
somukhannan <somukhannan8@gmail.com>
2021-07-02 15:04:12 +0000

    Logo box movement animation added.

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -3,10 +3,12 @@ import './index.css';
 
 const Logo = () =>
 	<div className="logo">
-		<div className="box b1 "/>
-		<div className="box b2 "/>
-		<div className="box b3 "/>
-		<div className="box b4 "/>
+		<div className="container">
+			<div className="box b1 "/>
+			<div className="box b2 "/>
+			<div className="box b3 "/>
+			<div className="box b4 "/>
+		</div>
 	</div>;
 
 export default Logo;
